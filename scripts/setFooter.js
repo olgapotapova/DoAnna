14b95aea6b38599816ba89245dc0b9ed4539c9e8
olgapotapova <olga6263@inbox.ru>
2023-02-11 02:03:59 +0200

    add changes

--- scripts/setFooter.js
+++ scripts/setFooter.js
@@ -1,17 +1,11 @@
-var footerIcons = `<div id="menu-right" class="menu-right">
-        <nav>
-            <ul>
-                <li><a href="about-me.html" class="lng-minust">Minust</a></li>
-                <li><a href="singing-studio.html" class="lng-vokalstudio">Laulastuudio</a></li>
-                <li><a href="game-test.html" class="lng-kuulamismäng">Kuulamismäng</a></li>
-                <li><a href="reviews.html" class="lng-tagasaside">Tagasaside</a></li>
-                <li><a href="contact.html" class="lng-kontakt">Kontakt</a></li>
-                <li><img src="/images/Pattern.svg" alt="Pattern from notes."></li>
-            </ul>
-        </nav>
-    </div>`;
+var footerIcons = `<div class="social-media">
+    <img class="icon" src="images/InstagramGold.svg" alt="Gold Instagram icon.">
+    <img class="icon-telegram" src="images/TelegramGold.svg" alt="Gold Telegram icon.">
+    <img class="icon" src="images/FacebookGold.svg" alt="Gold Facebook icon.">
+    <img class="icon" src="images/ViberGold.svg" alt="Gold Viber icon.">
+</div>`;
 
-var footerText = `<div class="footer-main-page">
+var footerText = `<div class="footer-page">
 <h3 class="telephone">Tel. +372 5561 40 84</h3>
 </div>`;
 
